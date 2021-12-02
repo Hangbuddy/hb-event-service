f414fdb6c9bb675dadc5ff81a9973ff84666f505
sahin <osumatu@gmail.com>
2021-12-02 23:11:01 +0100

    EventType migration is added

--- EventService.Api/Migrations/20211202221047_AddedEventType.Designer.cs
+++ EventService.Api/Migrations/20211202221047_AddedEventType.Designer.cs
@@ -4,15 +4,17 @@ using EventService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
 namespace EventService.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211202221047_AddedEventType")]
+    partial class AddedEventType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EventService.Api/Migrations/20211202221047_AddedEventType.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventService.Migrations
+{
+    public partial class AddedEventType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EventType",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventType",
+                table: "Events");
+        }
+    }
+}
